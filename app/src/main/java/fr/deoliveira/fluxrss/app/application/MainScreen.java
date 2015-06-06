4000efd49ed625ef55172a6d8aeae8c6edb319ba
Romain-De-Oliveira <romainpaulo.deoliveira@gmail.com>
2015-06-06 21:55:42 +0200

    mise en place nouveau NavigationView

--- app/src/main/java/fr/deoliveira/fluxrss/app/application/MainScreen.java
+++ app/src/main/java/fr/deoliveira/fluxrss/app/application/MainScreen.java
@@ -2,10 +2,14 @@ package fr.deoliveira.fluxrss.app.application;
 
 import android.content.res.Configuration;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -17,48 +21,97 @@ import fr.deoliveira.fluxrss.app.ajoutmanuel.AjoutFluxFragment;
 import fr.deoliveira.fluxrss.app.fluxrss.FluxRssFragment;
 import fr.deoliveira.fluxrss.app.itemrss.ItemRssFragment;
 
+
 /**
  * Created by Romain on 07/05/2015.
  */
 public class MainScreen extends AppCompatActivity implements FluxRssFragment.OnFluxRssInteractionListener,
 ItemRssFragment.OnItemRssInteractionListener,AjoutFluxFragment.OnAjoutFluxInteractionListener {
 
-    private FragmentNavigationDrawer dlDrawer;
+    //private FragmentNavigationDrawer dlDrawer;
+    private DrawerLayout dlDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.main_activity_2);
+        setContentView(R.layout.main_activity);
         // Set a Toolbar to replace the ActionBar.
-        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
         // Find our drawer view
-        dlDrawer = (FragmentNavigationDrawer) findViewById(R.id.drawer_layout);
-        LinearLayout rootDrawerView = (LinearLayout) findViewById(R.id.rootDrawer);
+        dlDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // Set the menu icon instead of the launcher icon.
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.rss25);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
         // Setup drawer view
-        dlDrawer.setupDrawerConfiguration((ListView) findViewById(R.id.lvDrawer), toolbar,
-                R.layout.drawer_list_item, R.id.flContent, rootDrawerView);
-        // Add nav items
-        dlDrawer.addNavItem("Mes Flux Rss",R.drawable.list30, "First Fragment", FluxRssFragment.class);
-        dlDrawer.addNavItem("Ajouter un Flux",R.drawable.plus25, "Second Fragment", AjoutFluxFragment.class);
-        //Comentaire que dans le master.
-        // Select default
-        if (savedInstanceState == null) {
-            dlDrawer.selectDrawerItem(0);
+        setupDrawerContent(nvDrawer);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the planet to show based on
+        // position
+        Fragment fragment = null;
+
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = FluxRssFragment.class;
+                break;
+            case R.id.nav_second_fragment:
+                fragmentClass = AjoutFluxFragment.class;
+                break;
+//            case R.id.nav_third_fragment:
+//                fragmentClass = ThirdFragment.class;
+//                break;
+            default:
+                fragmentClass = FluxRssFragment.class;
         }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        dlDrawer.closeDrawers();
     }
 
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content
-        if (dlDrawer.isDrawerOpen()) {
-            // Uncomment to hide menu items
-            // menu.findItem(R.id.mi_test).setVisible(false);
-        }
-        return super.onPrepareOptionsMenu(menu);
-    }
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        // If the nav drawer is open, hide action items related to the content
+//        if (dlDrawer.isDrawerOpen()) {
+//            // Uncomment to hide menu items
+//            // menu.findItem(R.id.mi_test).setVisible(false);
+//        }
+//        return super.onPrepareOptionsMenu(menu);
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -70,11 +123,11 @@ ItemRssFragment.OnItemRssInteractionListener,AjoutFluxFragment.OnAjoutFluxIntera
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-        if (dlDrawer.getDrawerToggle().onOptionsItemSelected(item)) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                dlDrawer.openDrawer(GravityCompat.START);
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -82,14 +135,11 @@ ItemRssFragment.OnItemRssInteractionListener,AjoutFluxFragment.OnAjoutFluxIntera
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         // Sync the toggle state after onRestoreInstanceState has occurred.
-        dlDrawer.getDrawerToggle().syncState();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggles
-        dlDrawer.getDrawerToggle().onConfigurationChanged(newConfig);
     }
 
     @Override
