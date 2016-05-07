ea5f6670eab341dc85b25991d9417d50fbc3ff24
Romain De Oliveira <romainpaulo.deoliveira@gmail.com>
2016-05-07 17:52:28 +0200

    test credentials

--- app/src/main/java/fr/deoliveira/fluxrss/app/base/DatabaseHelper.java
+++ app/src/main/java/fr/deoliveira/fluxrss/app/base/DatabaseHelper.java
@@ -12,7 +12,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "rssDb.db";
     private static final int DATABASE_VERSION = 5;
 
-
+//
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
